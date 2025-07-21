330a8c82a61fd367bd76537e7c9b9334d5e0e012
touseef <touseefktk22@gmail.com>
2025-07-22 02:36:50 +0500

    recurring order cancel done

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-\Illuminate\Support\Facades\Schedule::command('orders:process-recurring')->everyMinute();
+\Illuminate\Support\Facades\Schedule::command('orders:process-recurring')->dailyAt('00:05');
