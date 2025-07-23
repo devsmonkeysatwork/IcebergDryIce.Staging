c8c6638c48a8e05a5949c4ab83a0c09bd57fc1be
touseef <touseefktk22@gmail.com>
2025-07-23 21:44:25 +0500

    downlaods added

--- resources/views/website/blasting/blasting_manuals.blade.php
+++ resources/views/website/blasting/blasting_manuals.blade.php
@@ -15,10 +15,10 @@
                 <td>ColdJet 75</td>
                 <td rowspan='2'><img src='{{asset('website/images/cj75.png')}}'></td>
             </tr><tr><td valign="top">
-                    <a href='/downloads/cj75_manual.pdf' target='_blank'>Manual</a><br>
-                    <a href='/downloads/cj75_training.wmv' target='_blank'>Training</a><br>
-                    <a href='/downloads/cj75_parts.pdf' target='_blank'>Parts</a><br>
-                    <a href='/downloads/cj75_parts_applicator.pdf' target='_blank'>Applicator Parts</a></td>
+                    <a href='website/downloads/cj75_manual.pdf' target='_blank'>Manual</a><br>
+                    <a href='website/downloads/cj75_training.wmv' target='_blank'>Training</a><br>
+                    <a href='website/downloads/cj75_parts.pdf' target='_blank'>Parts</a><br>
+                    <a href='website/downloads/cj75_parts_applicator.pdf' target='_blank'>Applicator Parts</a></td>
 
             </tr>
         </table>
