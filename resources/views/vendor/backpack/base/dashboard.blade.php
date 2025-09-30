c161eab3ab1176072104973db15083b17ee7ab4c
Hamza Javed <100803405+HamzaCoder020@users.noreply.github.com>
2025-10-01 02:08:18 +0500

    select-2 form style fixed

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -568,13 +568,6 @@
     .table.mb-3 {
         margin-bottom: 1rem;
     }
-    .select2-container .select2-selection--single{
-        height : 34px !important;
-        padding-top: 7px;
-    }
-    .select2-container--default .select2-selection--single .select2-selection__arrow{
-        top: 4px !important;
-    }
 
 </style>
 
