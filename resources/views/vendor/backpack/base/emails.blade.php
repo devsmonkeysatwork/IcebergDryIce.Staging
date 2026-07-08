5d233c041335c8bfa17905281cbda3cefa7544e9
touseef <touseefktk22@gmail.com>
2026-07-09 03:07:35 +0500

    pickup emails

--- resources/views/vendor/backpack/base/emails.blade.php
+++ resources/views/vendor/backpack/base/emails.blade.php
@@ -19,8 +19,8 @@
                         </div>
                         <div class="col-md-3">
                             <div class="btn-group-vertical">
-                                <button class="btn btn-secondary email-temp-btn" data-location="Kuehne & Nagel" data-email="ange@monkeysatwork.dev" onclick="selectLocation(this)">Kuehne & Nagel</button>
-                                <button class="btn btn-secondary email-temp-btn" data-location="Praxair" data-email="hamza40javed@gmail.com" onclick="selectLocation(this)">Praxair</button>
+                                <button class="btn btn-secondary email-temp-btn" data-location="Kuehne & Nagel" data-email="vancouver@linde.com" onclick="selectLocation(this)">Kuehne & Nagel</button>
+                                <button class="btn btn-secondary email-temp-btn" data-location="Praxair" data-email="admin@icebergdryice.com" onclick="selectLocation(this)">Praxair</button>
                             </div>
                         </div>
                         <div class="col-md-9">
